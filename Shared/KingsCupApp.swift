06084a2ade592b891ac68c57c3c865555e516f39
Sherry Xu <sherryxu826@gmail.com>
2021-12-27 23:33:18 -0800

    Initial Commit

---
+++ Shared/KingsCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingsCupApp.swift
+//  Shared
+//
+//  Created by Sherry Xu on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct KingsCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
